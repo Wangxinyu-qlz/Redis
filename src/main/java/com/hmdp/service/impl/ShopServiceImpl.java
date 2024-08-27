1d7f1441a88235bb01063152a4dc18bbc4581ec7
qiaolezi <2463853170@qq.com>
2024-08-27 09:52:17 +0800

    解决缓存击穿（热点key问题，高并发下，重建困难的key失效）：互斥锁

--- src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
@@ -55,11 +55,12 @@ public class ShopServiceImpl extends ServiceImpl<ShopMapper, Shop> implements IS
 			return Result.ok(shop);
 		}
 		//追加判断是否为""
+		//解决缓存穿透
 		if (shopJson != null && shopJson.equals("")) {
 			return Result.fail("店铺不存在");
 		}
 
-		//TODO 实现缓存重建
+		//TODO 实现缓存重建，解决缓存击穿
 		//获取互斥锁
 		String lock = LOCK_SHOP_KEY + id;
 		Shop shop = null;
@@ -101,6 +102,7 @@ public class ShopServiceImpl extends ServiceImpl<ShopMapper, Shop> implements IS
 		if (id == null) {
 			return Result.fail("商铺id为空");
 		}
+		//更新数据库 -> 删除缓存
 		//update DB
 		updateById(shop);
 	    //del redis
