736c577a070a5fd7cad1e8613c05817c3deaf823
qiaolezi <2463853170@qq.com>
2024-08-29 10:17:44 +0800

    redisson 分布式锁 依赖导入 配置 测试使用示例

--- src/test/java/com/hmdp/HmDianPingApplicationTests.java
+++ src/test/java/com/hmdp/HmDianPingApplicationTests.java
@@ -4,6 +4,8 @@ import com.hmdp.service.IShopService;
 import com.hmdp.utils.RedisIdWorker;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -11,6 +13,7 @@ import javax.annotation.Resource;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = HmDianPingApplication.class)
@@ -19,6 +22,8 @@ class HmDianPingApplicationTests {
 	private IShopService service;
 	@Resource
 	private RedisIdWorker redisIdWorker;
+	@Resource
+	private RedissonClient redissonClient;
 	@Test
 	void testSaveShop() {
 		for (int i = 0; i < 15; i++) {
@@ -48,5 +53,20 @@ class HmDianPingApplicationTests {
 		countDownLatch.await();
 		long end = System.currentTimeMillis();
 		System.out.println("耗时："+ (end - start));
+	}
+
+	@Test
+	void testRedisson() throws Exception{
+		//获取锁（可重入），指定锁的名称
+		RLock lock = redissonClient.getLock("anyLock");
+		//获取锁的最大等待时间，锁自动释放时间，时间单位
+		boolean triedLock = lock.tryLock(1, 10, TimeUnit.SECONDS);
+		if(triedLock) {
+			try{
+				System.out.println("run sth...");
+			}finally {
+				lock.unlock();
+			}
+		}
 	}
 }
